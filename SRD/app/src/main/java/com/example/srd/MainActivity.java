9d8c9d338255c4900eabcf343dc9ce0d1718e765
Karthik <metakgb97@gmail.com>
2023-04-25 23:48:45 +0530

    Renaming the variables.

--- SRD/app/src/main/java/com/example/srd/MainActivity.java
+++ SRD/app/src/main/java/com/example/srd/MainActivity.java
@@ -9,22 +9,22 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-     private Button button1;
-     private Button button2;
+     private Button adminbutton;
+     private Button usersbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1=(Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        adminbutton = (Button) findViewById(R.id.adminbtn);
+        adminbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity2();
             }
 
         });
-        button2=(Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        usersbutton = (Button) findViewById(R.id.usersbtn);
+        usersbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openActivity2();
