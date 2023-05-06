6610fbdde82da3c7c5443781f2d11592bfdd4b7b
Karthik <EE211629@sasken.com>
2023-05-06 18:31:46 +0530

    Dialog for add and delete users

---
+++ SRD/app/src/main/java/com/example/srd/DeleteUsersViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.srd;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DeleteUsersViewHolder extends RecyclerView.ViewHolder {
+    View mView;
+    TextView emp_id,emp_name;
+    public DeleteUsersViewHolder(@NonNull View itemView) {
+        super(itemView);
+        emp_id = itemView.findViewById(R.id.delcard_eid);
+        emp_name = itemView.findViewById(R.id.delcard_ename);
+
+    }
+}
--- SRD/app/src/main/java/com/example/srd/MainActivity2.java
+++ SRD/app/src/main/java/com/example/srd/MainActivity2.java
@@ -2,13 +2,31 @@ package com.example.srd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private Button login;
+    private Button cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        login = (Button) findViewById(R.id.login_btn);
+        cancel = (Button) findViewById(R.id.login_cancel);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+    }
+
+    private void openActivity3() {
+        Intent intent=new Intent(this, MainActivity3.class);
+        startActivity(intent);
     }
 }
